7c5a4ea66e0ba772a0c42b56225690143c2aefed
Andy Fields <vrvt2018@leeds.ac.uk>
2025-03-26 00:21:24 +0000

    submitting

--- task2/grade_analyser.py
+++ task2/grade_analyser.py
@@ -34,8 +34,9 @@ filename = input("Enter filename of student file: ")
 
 with open(filename[:-4] + "_out.txt", "w") as output_file:
      with open(filename) as input_file:
-          for line in input_file[1:]:
+          for line in input_file:
                parts = line.split(",")
+               if parts[0] is "student_id": continue
                student_id = parts[0]
                total_grade = 0
                num_modules = 0
